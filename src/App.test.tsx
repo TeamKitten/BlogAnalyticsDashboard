98890e7c1d60375600491048a126ec77d4522e5a
TinyKitten <catwalk8205@gmail.com>
2020-05-01 12:45:42 +0900

    chores

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app name', () => {
   const wrapper = shallow(<App />);
   const h1Element = wrapper.find('h1');
   expect(h1Element.text()).toBe('Blog Analytics Dashboard');
